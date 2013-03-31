d8734793171b8120bf537c7bb09d95a0528107ea
Oren Benvenisti <orenbnv@gmail.com>
2013-03-31 17:28:57 -0400

    first push to git of MTA

---
+++ MTA/SubwayMap.rb
@@ -0,0 +1,6 @@
+
+SUBWAY = {
+"N line" => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+"L line" => ["8th", "6th", "Union Square", "3rd", "1st" ],
+"6 line" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+ }
